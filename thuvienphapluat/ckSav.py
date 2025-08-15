40138fc13a5ada77fedbefcf461fc46dd3757f83
quantrinh1004 <135189914+quantrinh1004@users.noreply.github.com>
2025-08-15 03:43:11 +0000

    add

--- thuvienphapluat/ckSav.py
+++ thuvienphapluat/ckSav.py
@@ -12,7 +12,7 @@ from retrying import retry
 from bs4 import BeautifulSoup
 
 # Cấu hình thư mục lưu
-field = "bomayhanhchinh_1"
+field = "bomayhanhchinh_3"
 download_dir = f"download_{field}"
 os.makedirs(download_dir, exist_ok=True)
 
@@ -87,7 +87,7 @@ def download_content(url, index):
                 EC.presence_of_element_located((By.CSS_SELECTOR, "div#tab1.contentDoc, div.col-md-12.py-4"))
             )
         except Exception:
-            screenshot_path = f"{download_dir}/error_file_{field}_{index + 544}.png"
+            screenshot_path = f"{download_dir}/error_file_{field}_{index + 697}.png"
             driver.save_screenshot(screenshot_path)
             logging.error(f"❌ Không tìm thấy nội dung chính tại {url} - đã lưu ảnh lỗi {screenshot_path}")
             return False
@@ -102,7 +102,7 @@ def download_content(url, index):
         text = soup.get_text(separator="\n", strip=True)
 
         doc_id = url.rstrip("/").split("/")[-1].split(".")[0]
-        file_path = os.path.join(download_dir, f"{field}_{index + 544}_{doc_id}.txt")
+        file_path = os.path.join(download_dir, f"{field}_{index + 697}_{doc_id}.txt")
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(text)
 
